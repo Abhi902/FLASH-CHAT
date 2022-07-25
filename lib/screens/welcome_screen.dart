8539536e6231d69d5002d95ce3d796dcd73e2735
Abhi902 <dangiabhishek336@gmail.com>
2022-07-25 20:42:15 +0530

    third commit

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static String id = 'welcome_screen';
@@ -17,11 +18,19 @@ class _WelcomeScreenState extends State<WelcomeScreen>
     super.initState();
     controller =
         AnimationController(vsync: this, duration: Duration(seconds: 2));
+
     controller!.forward();
-    controller!.addListener(() {
+    animation = CurvedAnimation(parent: controller!, curve: Curves.bounceInOut);
+
+    animation!.addListener(() {
       setState(() {});
-      print(controller!.value);
+      print(animation!.value);
     });
+  }
+
+  void dispose() {
+    controller!.dispose();
+    super.dispose();
   }
 
   Widget build(BuildContext context) {
@@ -39,16 +48,17 @@ class _WelcomeScreenState extends State<WelcomeScreen>
                   tag: 'logo',
                   child: Container(
                     child: Image.asset('images/logo.png'),
-                    height: controller!.value * 90,
+                    height: animation!.value * 97,
                   ),
                 ),
-                Text(
-                  'Flash Chat',
-                  style: TextStyle(
+                TypewriterAnimatedTextKit(
+                  text: ['Flash Chat'],
+                  textStyle: TextStyle(
                     fontSize: 45.0,
                     fontWeight: FontWeight.w900,
                     color: Colors.grey,
                   ),
+                  speed: Duration(milliseconds: 300),
                 ),
               ],
             ),
